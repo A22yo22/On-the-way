4f60e786a1116eff0784b7e4b048b41228c4c958
Unnamed_DEV <a22yo22io@gmail.com>
2022-07-19 14:07:24 +0200

    Shop lesggggggggggggggggggggggggggggggggggggggggggggggggggggggooooooooooooooooooooooooooooooooooooooooooooo
    header to the Stack shop and Scriptable Objects for all shop items

---
+++ OnTheWay/Assets/Scripts/ScriptableObjectSpawner/StackSkins.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StackSkins : ScriptableObject
+{
+    public List<Color> playerSkinColors;
+    public List<Sprite> playerSkinTextures;
+}
--- OnTheWay/Assets/Scripts/Stack/LoadSkins.cs
+++ OnTheWay/Assets/Scripts/Stack/LoadSkins.cs
@@ -8,10 +8,13 @@ public class LoadSkins : MonoBehaviour
     public GameObject Player1;
     public GameObject Player2;
 
-    public List<Color> playerSkinColors;
+    List<Color> playerSkinColors;
+
+    public StackSkins stackSkinsObject;
 
     void Start()
     {
+        playerSkinColors = stackSkinsObject.playerSkinColors;
         Player1.GetComponent<SpriteRenderer>().color = playerSkinColors[PlayerPrefs.GetInt("StackPlayerSkinSelected")];
         Player2.GetComponent<SpriteRenderer>().color = playerSkinColors[PlayerPrefs.GetInt("StackPlayerSkinSelected")];
     }
